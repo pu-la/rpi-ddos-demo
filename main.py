d79fe0f2f477bb75dbed22513809862481b697f4
Rienk <rienk@kuipers.org>
2021-11-16 22:25:21 +0200

    Update main.py
    Code actually does something now

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ options = "hip"
 long_options = ["help"]
 
 try:
-
-   arguments, values = getopt.getopt(argumentList, options, long_options)
-   for currentArgument, currentValue in arguments:
-       if currentArgument in ("-h", "--Help"):
-           print ("Please specify an ip address with -ip")            
-       elif currentArgument in ("-ip"):
-           print ("Displaying file_name:", sys.argv[0])
+	
+	arguments, values = getopt.getopt(argumentList, options, long_options)
+   	for currentArgument, currentValue in arguments:            
+		if currentArgument in ("-ip"):
+           		while True:
+				ddos(currentValue)
+		elif currentArgument in ("-h", "--Help"):
+           		print ("Please specify an ip address with -ip")
 
 except getopt.error as err:
     print (str(err))
