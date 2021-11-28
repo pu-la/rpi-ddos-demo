9bdce3e1eda60d94161f21af92f38ab24799a473
Rienk <rienk@kuipers.org>
2021-11-28 23:58:17 +0200

    Update install-sever.sh

--- install-sever.sh
+++ install-sever.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 mkdir ~/server
 mv ./../rpi-ddos-demo ~/server
-sudo apt-get install apache2 php7 libapache2-mod-php7.3
-cat /etc/apache2/sites-enabled/000-default | \
-sed -e 's/AllowOverride None/AllowOverride ALL/' > /etc/apache2/sites-enabled/000-default
+sudo apt-get install apache2
 sudo service apache2 restart
